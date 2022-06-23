6e29ff0cef325fd8c8b32eb0622caaf517c0c904
rohmat97 <rohmat661@gmail.com>
2022-06-23 14:06:57 +0700

    feat: initialization project using ignite andross, and configuration for project ios (flipper issues)

---
+++ ios/Pods/Target Support Files/Pods-rohmatnobi-rohmatnobiTests/Pods-rohmatnobi-rohmatnobiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rohmatnobi_rohmatnobiTests : NSObject
+@end
+@implementation PodsDummy_Pods_rohmatnobi_rohmatnobiTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-rohmatnobi/Pods-rohmatnobi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rohmatnobi : NSObject
+@end
+@implementation PodsDummy_Pods_rohmatnobi
+@end
